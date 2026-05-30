15db836bb18848ce77611e9db389811ced3dd2fe
abrar-mahir <abrar21mahir@gmail.com>
2026-05-30 17:50:07 +0600

    first commit

---
+++ Find_Minimum_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int my_array[] = {7, 12, 9, 4, 11};
+    int minVal = my_array[0];
+
+    for(int i = 0; i < 5; i++) {
+        if(my_array[i] < minVal) {
+            minVal = my_array[i];
+        }
+    }
+
+    cout << "Lowest value: " << minVal << endl;
+
+    return 0;
+}
