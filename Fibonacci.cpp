d593f67c39400f1ef3132cedbeaabcb6a255a773
abrar-mahir <abrar21mahir@gmail.com>
2026-05-30 17:58:33 +0600

    first commit

---
+++ Fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int prev2 = 0;
+    int prev1 = 1;
+
+    cout << prev2 << endl;
+    cout << prev1 << endl;
+
+    for (int i = 0; i < 18; i++) {
+        int newFibo = prev1 + prev2;
+        cout << newFibo << endl;
+        
+        prev2 = prev1;
+        prev1 = newFibo;
+    }
+
+    return 0;
+}
